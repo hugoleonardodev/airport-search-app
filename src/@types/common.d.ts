01d70b97dd7879b15e9733a1299acb26996dc09c
Hugo <hugos@xgrow.com>
2022-08-23 13:31:31 -0300

    re deploying

--- src/@types/common.d.ts
+++ src/@types/common.d.ts
@@ -1,6 +1,10 @@
 import { google } from 'google-maps'
 
-declare const google: google
+// eslint-disable-next-line @typescript-eslint/naming-convention
+declare interface Window {
+  // add you custom properties and methods
+  google: google
+}
 
 interface IContextProvider {
   children?: React.ReactNode
